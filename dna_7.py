036e32807aca3ac32ce04b151ee25d65946892ed
Pinky-cloud224 <pinkydebnath4@gmail.com>
2020-10-19 14:00:26 +0600

    1st

---
+++ dna_7.py
@@ -0,0 +1,37 @@
+user_dna_sequence = [
+[
+"Roksana Kozlova", 18, "GTTAGCCGACTTGGCXCTTT"
+],
+[
+"Roza Kiseleva", 19, "ATTTGGCGAATTGGCCTTT"
+],
+[
+"Kliment Volkov", 27, "ATTTGACCAATTGZGCAAA"
+],
+[
+"Afanas Morozov", 22, "ATTTGACCGATTNGGCAA"
+],
+]
+
+dna_elements = ["A", "T", "G", "C"]
+
+string_1=user_dna_sequence[0][2]
+for index in range(19,len(string_1)):
+    if string_1[19]=="T":
+
+        print(user_dna_sequence[0])
+
+string_2=user_dna_sequence[1][2]
+for index in range(18,len(string_2)):
+    if string_2[18]=="T":
+        print(user_dna_sequence[1])
+
+string_3=user_dna_sequence[2][2]
+for index in range(18,len(string_3)):
+    if string_3[18]=="T":
+        print(user_dna_sequence[2])
+
+string_4=user_dna_sequence[3][2]
+for index in range(17,len(string_4)):
+    if string_4[17]=="T":
+        print(user_dna_sequence[3])
